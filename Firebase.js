6665e1c21d6f07a2da15f5de9874e40ce8108202
Andrew <andrewborondia@student.mitt.ca>
2021-06-25 15:54:12 -0500

    add redirects file

--- Firebase.js
+++
@@ -1,8 +0,0 @@
-var firebaseConfig = {
-  apiKey: "AIzaSyDRGBweih5vJEYxbLcx8WzbuTaDJOpPooA",
-  authDomain: "sd-260-firebase.firebaseapp.com",
-  projectId: "sd-260-firebase",
-  storageBucket: "sd-260-firebase.appspot.com",
-  messagingSenderId: "1070192714094",
-  appId: "1:1070192714094:web:dcd85107eca64fbf8de17f"
-};
